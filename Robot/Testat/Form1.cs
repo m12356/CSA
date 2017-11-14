f485c83fe57f03806a610837f73b8dc3d8d800f9
Martin Gruber <martin.gruber@stud.hslu.ch>
2017-11-14 20:34:06 +0100

    blub

--- Robot/Testat/Form1.cs
+++ Robot/Testat/Form1.cs
@@ -13,11 +13,48 @@ namespace Testat
 {
     public partial class Form1 : Form
     {
+        private Drive drive;
+        RobotConsole robotConsole;
+
         public Form1()
         {
             InitializeComponent();
+            this.robotConsole = new RobotCtrl.RobotConsole();
+            this.consoleView.RobotConsole = robotConsole;
+            robotConsole[Switches.Switch1].SwitchStateChanged += Form1_SwitchStateChanged;
+            robotConsole[Switches.Switch2].SwitchStateChanged += Form1_SwitchStateChanged;
+            robotConsole[Switches.Switch3].SwitchStateChanged += Form1_SwitchStateChanged;
+            robotConsole[Switches.Switch4].SwitchStateChanged += Form1_SwitchStateChanged;
+            this.commonRunParameters.SpeedChanged += CommonRunParameters1_SpeedChanged;
+            this.commonRunParameters.AccelerationChanged += CommonRunParameters1_AccelerationChanged;
+            this.drive = new Drive();
+            this.drive.Power = true;
+            this.driveView.Drive = drive;
+            this.runLine.Drive = drive;
+            this.runArc.Drive = drive;
+            this.runTurn.Drive = drive;
+
+            CommonRunParameters1_AccelerationChanged(null, EventArgs.Empty);
+            CommonRunParameters1_SpeedChanged(null, EventArgs.Empty);
+        }
+        private void CommonRunParameters1_AccelerationChanged(object sender, EventArgs e)
+        {
+            this.runLine.Acceleration = this.commonRunParameters.Acceleration;
+            this.runArc.Acceleration = this.commonRunParameters.Acceleration;
+            this.runTurn.Acceleration = this.commonRunParameters.Acceleration;
         }
 
+        private void CommonRunParameters1_SpeedChanged(object sender, EventArgs e)
+        {
+            this.runLine.Speed = this.commonRunParameters.Speed;
+            this.runArc.Speed = this.commonRunParameters.Speed;
+            this.runTurn.Speed = this.commonRunParameters.Speed;
+        }
+        private void Form1_SwitchStateChanged(object sender, SwitchEventArgs e)
+        {
+            robotConsole[(Leds)(int)e.Swi].LedEnabled = e.SwitchEnabled;
+
+        }
 
     }
 }
